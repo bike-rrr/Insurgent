30c8dec76b64ab55f56a39c6d259409da31438bf
bike-rrr <tesla201299@gmail.com>
2021-09-02 13:24:47 -0500

    didnt add anything i think....?

--- app.js
+++ app.js
@@ -6,29 +6,31 @@ const URL = 'https://www.evil-bikes.com/a/bikes/insurgent';
 
 async function scrape() {
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto('https://www.evil-bikes.com/a/bikes/insurgent')
+    {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto('https://www.evil-bikes.com/a/bikes/insurgent')
 
-    const sizesAvailability = await page.evaluate(() => {
-        const sizeList = document.querySelector("#size");
+        const sizesAvailability = await page.evaluate(() => {
+            const sizeList = document.querySelector("#size");
 
-        if (sizeList) {
-            const sizes = [];
-            for (let i = 0; i < sizeList.length; i++) {
-                sizes.push(sizeList[i].innerText)
+            if (sizeList) {
+                const sizes = [];
+                for (let i = 0; i < sizeList.length; i++) {
+                    sizes.push(sizeList[i].innerText)
+                }
+                const reFormatted = sizes.map(item => item = item.replace(/\s+/g, ' ').trim().toLowerCase());
+                return reFormatted;
+
+            } else {
+                return "couldnt queryselect element";
             }
-            const reFormatted = sizes.map(item => item = item.replace(/\s+/g, ' ').trim().toLowerCase());
-            return reFormatted;
+        })
+        await browser.close();
 
-        } else {
-            return "couldnt queryselect element"
-        }
-    })
-    await browser.close();
-
-    return sizesAvailability;
+        return sizesAvailability;
+    }
 }
 
 let inventory = ["fasle", "false", "false", "false"];
@@ -57,9 +59,8 @@ async function main() {
             setTimeout(main, 10000);
             console.log('--------------------')
         }
-        textMessage("booya")
     }).catch((err) => console.log(err))
+    //console og
 }
-
 // run program
 main()
