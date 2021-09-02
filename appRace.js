05d7f8891fd4a4f6dc922b24f031936d23671bfa
bike-rrr <tesla201299@gmail.com>
2021-09-02 13:23:53 -0500

    setup a Promise Race for the scrape

---
+++ appRace.js
@@ -0,0 +1,82 @@
+const puppeteer = require("puppeteer")
+const vonageFunctions = require('./vonage.js');
+const textMessage = vonageFunctions.textNumber;
+
+const URL = 'https://www.evil-bikes.com/a/bikes/insurgentt';
+
+async function scrape() {
+
+    const getData = new Promise(async (resolve, reject) => {
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto('https://www.evil-bikes.com/a/bikes/insurgent')
+
+        const sizesAvailability = await page.evaluate(() => {
+            const sizeList = document.querySelector("#size");
+
+            if (sizeList) {
+                const sizes = [];
+                for (let i = 0; i < sizeList.length; i++) {
+                    sizes.push(sizeList[i].innerText)
+                }
+                const reFormatted = sizes.map(item => item = item.replace(/\s+/g, ' ').trim().toLowerCase());
+                return reFormatted;
+            }
+        })
+        await browser.close();
+
+        if (sizesAvailability) {
+            resolve(sizesAvailability)
+        } else {
+            reject("wrong url boiiis")
+        }
+    });
+
+    const time_Limit = new Promise(resolve => {
+        setTimeout(() => {
+            resolve('too slow')
+        }, 10000)
+    });
+
+    return Promise.race([getData, time_Limit]);
+}
+
+let inventory = ["fasle", "false", "false", "false"];
+
+async function main() {
+
+    scrape().then((resp) => {
+        if (resp === "couldnt queryselect element") {
+            console.log('query failed');
+            console.log('--------------------')
+            setTimeout(main, 5000);
+        }
+        else if (resp === "too slow") {
+            console.log("too slow");
+            console.log('--------------------')
+            main();
+        }
+        // check each size
+        else {
+            for (let i = 0; i < resp.length; i++) {
+                if (resp[i].includes("out of stock")) {
+                    console.log(resp[i]);
+                }
+                else if (inventory[i] === "false") {
+                    console.log("in stock")
+                    textMessage('https://www.evil-bikes.com/a/bikes/insurgent');
+                    inventory[i] = true;
+                }
+            }
+            setTimeout(main, 5000);
+            console.log('--------------------')
+        }
+    }).catch((err) => {
+        console.log('we fucked ups', err);
+        main();
+    })
+}
+// run program
+main()
